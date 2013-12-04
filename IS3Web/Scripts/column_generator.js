46ab37b89754f11094df217e1fd62fc681cb2124
Radko Kotev <radkokotev@gmail.com>
2013-12-04 03:08:53 +0000

    Using bold HTML instead of sophisticated styling like @antonbelev

--- IS3Web/Scripts/column_generator.js
+++ IS3Web/Scripts/column_generator.js
@@ -1,13 +1,13 @@
 ﻿function generateColumns() {
     var html = "";//style=\"width: 275px; float: left;\"
-    var li = "<li class=\"ui-li ui-li-static ui-btn-up-c\"><div class=\"ui-checkbox\"><input id=\"";
+    var li = "<li class=\"ui-li ui-li-static ui-btn-up-c\"><table><tr><td width='100%'><div class=\"ui-checkbox\"><input id=\"";
     var jqmBottom1 = " data-corners=\"true\" data-shadow=\"false\" data-iconshadow=\"true\" data-wrapperels=\"span\" data-icon=\"checkbox-off\" data-theme=\"c\" data-mini=\"true\"><span class=\"ui-btn-inner ui-btn-corner-all\"><span class=\"ui-btn-text\">"
-    var jqmBottom2 = "</span><span class=\"ui-icon ui-icon-checkbox-off ui-icon-shadow\"> </span></span></label></div>";
+    var jqmBottom2 = "</span><span class=\"ui-icon ui-icon-checkbox-off ui-icon-shadow\"> </span></span></label></div></td><td width='30%'>";
     var uiSelector = $('.ulColumns');
     var invSlider1 = "<div class=\"invSlider\"><select name=\""; //style=\"float: right; \"
     var invSlider2 = "\" id=\"";
-    var invSlider3 = "\" data-role=\"slider\" class=\"invSlider ui-slider-switch\" data-mini=\"true\"><option value=\"off\">Off</option>";
-    var invSlider4 = "<option value=\"on\">On</option></select></div>"; //close div
+    var invSlider3 = "\" data-role=\"slider\" class=\"invSlider ui-slider-switch\" data-mini=\"true\"><option value=\"same\">Same</option>";
+    var invSlider4 = "<option value=\"inv\">Inv</option></select></div>"; //close div
     var result = $.parseJSON(JSON.stringify(json[0], undefined, 2));
     $.each(result, function (k, v) {
         //display the key and value pair
@@ -22,7 +22,7 @@
             + invSlider2 + k + "Sld"
             + invSlider3
             + invSlider4
-            + "</li>";
+            + "</td></tr></table></li>";
     });
     uiSelector.append(html).trigger('create');
 }
